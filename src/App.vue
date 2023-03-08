3fa5da187a25182f5c29966c09bd7d787188def3
jett <jettzhang95@gmail.com>
2023-03-08 22:18:47 +0800

    refactor: move dark mode listener to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { watch } from 'vue'
+import store from '@/stores'
+import { systemDarkModeMediaQuery } from '@/utils'
+
+watch(
+  () => store.theme,
+  (newTheme) => {
+    if (
+      newTheme === 'dark' ||
+      (newTheme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+
+    if (newTheme === 'system') {
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', systemDarkModeMediaQuery)
+    } else {
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .removeEventListener('change', systemDarkModeMediaQuery)
+    }
+  },
+  {
+    immediate: true
+  }
+)
 </script>
 
 <template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,5 @@
 <script setup lang="ts">
-import { watch } from 'vue'
 import FunctionIcons from '@/components/function-icons/FunctionIcons.vue'
-import store from '@/stores'
-import { systemDarkModeMediaQuery } from '@/utils'
-
-watch(
-  () => store.theme,
-  (newTheme) => {
-    if (
-      newTheme === 'dark' ||
-      (newTheme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)
-    ) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-
-    if (newTheme === 'system') {
-      window
-        .matchMedia('(prefers-color-scheme: dark)')
-        .addEventListener('change', systemDarkModeMediaQuery)
-    } else {
-      window
-        .matchMedia('(prefers-color-scheme: dark)')
-        .removeEventListener('change', systemDarkModeMediaQuery)
-    }
-  },
-  {
-    immediate: true
-  }
-)
 </script>
 
 <template>
